5d6d8a329d4553bde38290cb17e2fde10d20898f
Stefan Dumitrescu <dumitrescu.stefan@gmail.com>
2023-06-02 23:08:56 +0000

    third

--- run.py
+++ run.py
@@ -2,16 +2,18 @@ import subprocess
 import os, shutil, click
 
 @click.command()
-@click.option("--folder", default="../../../data/mc4/0_raw", help="Destination folder where to write")
-@click.option("--temp", default="../../../_temp", help="Temporary folder used to extract archives")
-@click.option("--overwrite", is_flag=True, help="Clean and start from scratch")
+@click.option("--folder", default="./data/", help="Destination folder where to write")
+@click.option("--temp", default="./temp", help="Temporary folder used to extract archives")
 
-def main(folder, temp, overwrite):
+def main(folder, temp):
     folder = os.path.abspath(folder)
     temp = os.path.abspath(temp)
 
     print("Install git-lfs")
     subprocess.run(['sudo', 'apt', 'install', 'git-lfs'])
+
+    print("Set GIT_LFS_SKIP_SMUDGE=1 environment variable")
+    os.environ['GIT_LFS_SKIP_SMUDGE'] = '1'
 
     print("Clone the repository")
     subprocess.run(['git', 'clone', 'https://huggingface.co/datasets/allenai/c4'])
@@ -19,31 +21,20 @@ def main(folder, temp, overwrite):
     print("Change directory to c4")
     os.chdir('c4')
 
-    print("Set GIT_LFS_SKIP_SMUDGE=1 environment variable")
-    os.environ['GIT_LFS_SKIP_SMUDGE'] = '1'
+    #print("Set GIT_LFS_SKIP_SMUDGE=1 environment variable")
+    #os.environ['GIT_LFS_SKIP_SMUDGE'] = '1'
 
     print("Pull LFS files")
     subprocess.run(['git', 'lfs', 'pull', '--include', 'multilingual/c4-ro.*.json.gz'])
+    subprocess.run('mv', 'multilingual/c4-ro.*.json.gz', temp)
 
     print("Go back to the previous directory")
     os.chdir('..')
 
-    if overwrite:
-        print(f"==== [ OVERWRITING ] ====")
-
-    if os.path.exists(folder):
-        if not overwrite:
-            print(f"\nFolder {folder} already exists, not doing anything.\n")
-            #return
-        else:  
-            print("Cleanup...")
-            shutil.rmtree(folder, ignore_errors=True)
-            shutil.rmtree(temp, ignore_errors=True)
-
     os.makedirs(folder, exist_ok=True)
     os.makedirs(temp, exist_ok=True)
 
-    print("Downloading MC4 dataset...")
+    print("Extract MC4 dataset...")
     subprocess.run(['python3', 'c4_extractor.py', '--data_dir', temp, '--output_dir', folder])
     
     print("Cleaning the c4 repo...")
